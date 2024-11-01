d6e5a737f59966562d04e8845a81d3d60e6281b6
Miha <luksic.miha@gmail.com>
2024-11-01 19:54:52 +0100

    cicd: make tests pass

--- holidays_test.go
+++ holidays_test.go
@@ -16,7 +16,7 @@ func TestReadHolidaysFile(t *testing.T) {
 
 	want := map[string]string{
 		"2024-01-01": "novo leto",
-		"2024-01-02": "novo leto testtest",
+		"2024-01-02": "novo leto test",
 	}
 
 	reader := strings.NewReader(records)
